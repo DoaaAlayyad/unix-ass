b2026cebeaee83dae53290fc4a2569dafab6482b
DoaaAlayyad <alayyadoaa@gmail.com>
2025-01-08 20:43:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,16 @@ if ($mysqli->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $word = $_POST['word'];
-    $query = $mysqli->prepare("SELECT definition FROM words WHERE word = ?");
+    $query = $mysqli->prepare("SELECT definition = ?");
     $query->bind_param("s", $word);
     $query->execute();
     $result = $query->get_result();
     
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-        echo "Definition: " . htmlspecialchars($row['definition']);
+        echo "def: " . htmlspecialchars($row['definition']);
     } else {
-        echo "Word not found!";
+        echo "not found!";
     }
 }
 ?>
